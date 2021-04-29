e2966022993fa085f9213d6495cf1944dd610f0f
fatimakhatungit <fatimakhatuncse@gmail.com>
2021-04-29 16:24:52 +0600

    added

---
+++ letconst.js
@@ -0,0 +1,19 @@
+const hubby = "Elias Kanchon";
+hubby = "Omar sunny";
+console.log(hubby);
+
+const numbers = [12, 45];
+numbers[1] = 88;
+numbers.push(12);
+const nayok = {name:"sakib khan", phone:2374};
+numbers = ["sunny", "salman", "omit hasan"];
+// console.log(numbers);
+
+let patientName ="rohim chacha";
+patientName ="fatima";
+console.log(patientName);
+let sum = 0;
+for(var i = 0; i < 10; i++){
+    sum = sum + 1;
+}
+console.log(i);
